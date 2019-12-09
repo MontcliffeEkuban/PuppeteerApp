7bd45ee0b3fbab57b260ecf1b10df5626cbc38a1
M.Ekuban <gidxn16564767dsj@live.com>
2019-12-09 16:59:31 +0800

    Update creds.js

--- creds.js
+++ creds.js
@@ -1,5 +1,5 @@
 module.exports = {
-    user000: {username: "2155880500@qq.com",
-        password: "realtime123"}
+    user000: {username: "**********@qq.com",
+        password: "***********"}
     
 }
